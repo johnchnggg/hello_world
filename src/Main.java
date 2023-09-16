e877ef5aba2dbd197f3aaae2420101c59d2bfa0a
johnchnggg <113963908+johnchnggg@users.noreply.github.com>
2023-09-16 16:02:25 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 import java.util.Scanner;
 public class Main {
-
-    public static void oddOrEven(int k) {
-
-        if (k % 2 == 0) {
-            System.out.println(k + " is even.");
-        }
-
-        else {
-            System.out.println(k + " is odd.");
-        }
-    }
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("Enter integer: ");
-
-        while (true) {
-            int num = myScanner.nextInt();
-            if (num == -1) {
-                break;
-            }
-            else {
-                oddOrEven(num);
-            }
-        }
+        System.out.println("Hello World!");
     }
 }
